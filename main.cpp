00b9e32b04865da3caeb418866934459a3258712
meokisama <hoangdinhsang123@gmail.com>
2021-03-25 12:47:07 +0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include "widget.h"
+
+#include <QApplication>
+#include <QGraphicsDropShadowEffect>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Widget w;
+
+    //Hide title bar
+    w.setWindowFlags(Qt::Window | Qt::FramelessWindowHint);
+    //Make widget transparent
+    w.setAttribute(Qt::WA_TranslucentBackground);
+
+    // Add shadow effect
+    QGraphicsDropShadowEffect *effect = new QGraphicsDropShadowEffect();
+    effect->setXOffset(2);
+    effect->setYOffset(2);
+    effect->setBlurRadius(10);
+    effect->setColor(QColor(180, 180, 180, 180));
+    w.setGraphicsEffect(effect);
+
+    w.show();
+
+    return a.exec();
+}
